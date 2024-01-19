d818e681ac3a2c0428b3f0d6541186783efb2343
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2024-01-18 20:17:48 -0500

    create Chainable Route Handler

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,26 +2,13 @@ const express = require("express");
 const router = express.Router()
 const {createIndex, showPage, postBlog, aboutBlog, createBlog, redirectBlog, downloadImage} = require("../controllers/blogsController.js")
 
-
-//Index Page
-router.get("/", createIndex);
- 
-//Show a Blog
-router.get("/show", showPage);
-  
-//Add a Blog
-router.post("/", postBlog);
-
-//About the blog
-router.get("/about", aboutBlog);
-
-//Create new Blog
-router.get("/create", createBlog);
-
-//Redirect a Blog
-router.get("/about-us", redirectBlog);
-
-//Download an Image
-router.get("/download-file", downloadImage);
+router
+.get("/", createIndex)
+.get("/show", showPage)
+.post("/", postBlog)
+.get("/about", aboutBlog)
+.get("/create", createBlog)
+.get("/about-us", redirectBlog)
+.get("/download-file", downloadImage);
 
 module.exports = router
